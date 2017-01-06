c5aa3176cf17348ffb25b9ed58199570c1dbe099
coderussellville <coderussellville@gmail.com>
2017-01-06 13:49:37 -0600

    Split out header and footer files.

--- index.php
+++ index.php
@@ -1,67 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Family Tracker</title>
-  <!-- Add modernizer? -->
-  <link href="css/bootstrap.min.css" rel="stylesheet" />
-  <link href="css/font-awesome.min.css" rel="stylesheet" />
-</head>
-<body>
-  <header id="top">
-    <nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-navbar-collapse" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Memories</a>
-    </div>
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse navbar-right" id="main-navbar-collapse">
-      <ul class="nav navbar-nav">
-          <li>
-            <a href="#top" title="Code Russellville"><i class="fa fa-home"></i> Home</a>
-          </li>
-          <li>
-            <a href="#about" title="About Code Russellville Web Design"><i class="fa fa-user"></i> About</a>
-          </li>
-          <li>
-            <a href=#services title="Web design and development reporting databases training"><i class="fa fa-cogs"></i> Services</a>
-          </li>
-          <li>
-            <a href="#portfolio" title="Code Russellville Web design and development examples"><i class="fa fa-file-code-o"></i> Portfolio</a>
-          </li>
-          <li>
-            <a href="#contact" title="Contact Code Russellville for your web design and development needs"><i class="fa fa-phone"></i> Contact</a>
-          </li>
-        </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-  </header>
-  <main>
+<?php require 'templates/header.php';?>
   <div class="container-fluid">
     <div class="row">
       <div class="col-md-12">
         <?php echo "Content goes here." ?>
       </div>
     </div>
-    <div class="row">
-      <div class="col-md-12">
-        <footer>
-          <p>&copy; <?php echo date("Y"); ?> </p>
-        </footer>
-      </div>
-    </div>
   </div>
-  </main>
-  <script src="js/jquery-2.2.1.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-</body>
-</html>
+<?php require 'templates/footer.php';?>
---
+++ templates/footer.php
@@ -0,0 +1,14 @@
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <footer>
+            <p>&copy; <?php echo date("Y"); ?> </p>
+          </footer>
+        </div>
+      </div>
+    </div>
+  </main>
+  <script src="js/jquery-2.2.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
