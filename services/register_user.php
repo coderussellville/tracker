6a2e705828c9deab46839300eaf4cc405c2f3724
coderussellville <coderussellville@gmail.com>
2017-01-08 00:25:02 -0600

    Updated register_user.php.

--- services/register_user.php
+++ services/register_user.php
@@ -4,7 +4,7 @@
      Run validation checks
      IF everything is successful
        --> Create user in database
-       --> log user in (encrypt user_id?, set any session or cookie values?)
+       --> log user in (encrypt user_id?, set any session or cookie values?)  
        --> Take them to the next page (also send them a welcome email, etc.)
   */
   
